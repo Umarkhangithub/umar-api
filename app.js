df076ce9433494ed51824a4a86f8e171298caf78
Umarkhangithub <uk1941404@gmail.com>
2025-05-05 16:59:07 +0530

    umar

--- app.js
+++ app.js
@@ -1,30 +1,49 @@
 import express from 'express';
 import cors from 'cors';
+
 import authRoutes from './routes/authRoutes.js';
 import contactRoutes from './routes/contactRoutes.js';
 import projectRoutes from './routes/projectRoutes.js';
 import servicesRoutes from './routes/servicesRoutes.js';
-import { generalLimiter } from './middlewares/rateLimitMiddleware.js';
 import testimonialsRoutes from './routes/testimonialRoutes.js';
 import skillsRoutes from './routes/skillsRoutes.js';
 import profileRoutes from './routes/profileRoutes.js';
 
+import { generalLimiter } from './middlewares/rateLimitMiddleware.js';
+import connectDB from './config/db.js';
+
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Middlewares
+// ==================
+// Global Middlewares
+// ==================
 app.use(cors());
-app.use(express.json()); // Built-in middleware to handle JSON bodies
-app.use(express.urlencoded({ extended: true })); // Built-in middleware to handle URL-encoded data
-// app.use(generalLimiter); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(generalLimiter); // Uncomment in production
 
-
-// Routes
+// ==============
+// API Routes
+// ==============
 app.use("/api/admin", authRoutes);
-app.use('/api/contact', contactRoutes);
-app.use('/api/projects', projectRoutes);
-app.use('/api/services', servicesRoutes); 
-app.use('/api/skills', skillsRoutes); 
-app.use('/api/profile', profileRoutes); 
-app.use('/api/testimonials', testimonialsRoutes); 
+app.use("/api/contact", contactRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/services", servicesRoutes);
+app.use("/api/skills", skillsRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/api/testimonials", testimonialsRoutes);
 
-export default app;
+// ==============
+// 404 Handler
+// ==============
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// ==============
+// Start Server
+// ==============
+
+export default app
